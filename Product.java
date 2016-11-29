144a25a556569bce1dc44abda91b7f2c6dcd73d4
elecengabdallah <elecengabd_allah@yahoo.com>
2016-11-29 14:45:16 +0200

    update product class
    update product class to be used in data analysis and recording and in
    javafx table view

--- Product.java
+++ Product.java
@@ -1,29 +1,52 @@
+import javafx.beans.property.SimpleLongProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 public class Product {
-		String code;
-		int goodReads;
-		int badReads;
-		
-		public Product(String code) {
-			this.code = code;
-		}
-		
-		public void setGoodReads(int i) {
-			goodReads = goodReads + i;
-		}
-		
-		public void incrementBadReads() {
-			badReads++;
-		}
-		
-		public int getGoodReads() {
-			return goodReads;
-		}
-		
-		public int getBadReads() {
-			return badReads;
-		}
-		
-		public void resetBadReads() {
-			badReads = 0;
-		}
+	
+	
+	private SimpleStringProperty code;
+	private SimpleLongProperty startTime;
+	private SimpleLongProperty stopTime;
+	private SimpleLongProperty totalGood;
+	private SimpleLongProperty totalBad;
+	
+	public Product(String code, long time) {
+		this.code = new SimpleStringProperty(code);
+		this.startTime = new SimpleLongProperty(time);
+		this.stopTime = new SimpleLongProperty(System.currentTimeMillis());
+		this.totalGood = new SimpleLongProperty(0);
+		this.totalBad = new SimpleLongProperty(0);
 	}
+	
+	public String getCode() {
+		return code.get();
+	}
+	
+	public long getStartTime() {
+		return startTime.get();
+	}
+	
+	public void setStopTime(long time) {
+		stopTime.set(time);
+	}
+	
+	public long getStopTime() {
+		return stopTime.get();
+	}
+	
+	public void addTotalGood(int c) {
+		totalGood.add(c);
+	}
+	
+	public long getTotalGood() {
+		return totalGood.get();
+	}
+	
+	public void addTotalBad(int c) {
+		totalBad.add(c);
+	}
+	
+	public long getTotalBad() {
+		return totalBad.get();
+	}
+}
